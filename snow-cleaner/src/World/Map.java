f666a4f90d2aac00cec01945e045f03b5b0282f8
krypton17 <maxdodon25@gmail.com>
2018-12-08 17:42:31 +0200

    first commit

---
+++ snow-cleaner/src/World/Map.java
@@ -0,0 +1,14 @@
+package World;
+
+import java.util.ArrayList;
+
+public class Map {
+
+    final Float Scale = 1.0F;
+    private Integer width;
+    private Integer height;
+    private Double longitude = 0.0;
+    private Double latitude = 0.0;
+
+    private ArrayList<ArrayList <Sector>> grid;
+}
