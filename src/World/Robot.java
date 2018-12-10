508fa1b6fbe4b50e1ab224f4dc60e4c9b76ecd4e
dorinesinenco <dorin.e.art@gmail.com>
2018-12-10 20:59:13 +0200

    Create Robot.java

---
+++ src/World/Robot.java
@@ -0,0 +1,17 @@
+public class Robot{
+  
+  private Sector location; // gde on nahoditsa
+  private String name;   
+  
+  
+
+  // constructor,toString(),sets/gets
+  
+  // moveNorth(),moveEast(),moveSouth(),moveWest()
+  // if/ BOUNDARIES!!!!
+  
+  // 1) sozdati 1 robota
+  // 2) set -> sector
+  // 3) N N N E E S S W
+  // 4) print map
+}
