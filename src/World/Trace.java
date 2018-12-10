982350a2e37f02eaca606d3df24ce33e19818f9d
dorinesinenco <dorin.e.art@gmail.com>
2018-12-10 20:29:29 +0200

    Create Trace.java

---
+++ src/World/Trace.java
@@ -0,0 +1,25 @@
+package World;
+public class Trace{
+  final public byte N = 1;
+  final public byte E = 2;
+  final public byte S = 3;
+  final public byte W = 4;
+  
+  private byte direction;
+  
+  public Trace(byte direction){
+    setDirection(direction);
+  }
+  
+  public void setDirection(byte direction){
+     this.direciton = direciton;
+  }
+
+  public byte getDirection(){
+     return this.direciton;
+  }
+  
+  @Override 
+  
+
+}
