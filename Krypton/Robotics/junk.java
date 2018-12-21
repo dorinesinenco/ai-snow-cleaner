da02583c328a3f06b829a629e1267474c76bebc1
blacksw <36705765+blacksw@users.noreply.github.com>
2018-12-21 19:47:23 +0200

    Update junk.java

--- Krypton/Robotics/junk.java
+++ Krypton/Robotics/junk.java
@@ -90,3 +90,49 @@ public class Main {
         printArrRectangles(arr,2,4,6);
         }
     }
+
+
+
+///////////////////////////////////////////
+
+
+
+public class App {
+
+    public static void printArray( int[][] array ) {
+        int i, j;
+        for (i=0; i < (array.length); i++) {
+            for (j=0; j < (array[i].length); j++)
+                System.out.print(array[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void setValueInRadius(int[][] array, int x_position, int y_position, int radius, int value) {
+
+        for (int i=0; i < (radius*2+1); i++)
+            array[y_position - radius + i][x_position - radius] = value;
+
+        for (int j=0; j < (radius*2+1); j++)
+            array[y_position + radius][x_position - radius + j] = value;
+
+        for (int i=0; i < (radius*2+1); i++)
+            array[y_position + radius - i][x_position + radius] = value;
+
+        for (int j=0; j < (radius*2+1); j++)
+            array[y_position - radius][x_position + radius - j] = value;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+
+        for (int i=0; i < (array.length); i++) {
+            for (int j = 0; j < (array[i].length); j++)
+                array[i][j] = 1;
+        }
+        printArray( array );
+        setValueInRadius(array,2,3,1,0);
+        printArray( array );
+    }
+}
