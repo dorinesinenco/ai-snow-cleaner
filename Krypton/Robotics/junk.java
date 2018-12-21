002255b45ad5810cb51f4f483d3fa916930ea13c
blacksw <36705765+blacksw@users.noreply.github.com>
2018-12-21 19:41:22 +0200

    Update junk.java

--- Krypton/Robotics/junk.java
+++ Krypton/Robotics/junk.java
@@ -1,1 +1,92 @@
-sdadas
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void printArr(String[][] s){
+        for(int i = 0;i < s.length;i++){
+            for(int j = 0;j < s.length;j++){
+
+                    s[i][j] = " # ";
+
+            }
+        }
+        s[2][2] = " 0 ";
+            ///////////////////////
+        for(int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s.length; j++){
+
+                    System.out.print(s[i][j]);
+                }
+            System.out.println();
+        }
+        System.out.println("\n");
+    }
+
+    public static void printArrRectangle(String[][] s,int r,int pos){
+        for(int i = 0;i < s.length;i++){
+            for(int j = 0;j < s.length;j++){
+
+                s[i][j] = " # ";
+            }
+        }
+        s[5][5] = " 0 ";
+            ///////////////////////
+        int  counter = -r+1;
+            for(int i = -r+1;i < r;i++){
+                counter++;
+                s[pos-r][pos-i] = " x ";
+                s[pos+r-2][pos-i] = " x ";
+                s[pos-counter][pos+r-1] = " s ";
+                s[pos-counter][pos-r+1] = " s ";
+                }
+
+        for(int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s.length; j++){
+
+                System.out.print(s[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n\n");
+    }
+
+    public static void printArrRectangles(String[][] s,int r,int posX,int posY){
+        for(int i = 0;i < s.length;i++){
+            for(int j = 0;j < s.length;j++){
+
+                s[i][j] = " # ";
+            }
+        }
+        s[5][5] = " 0 ";
+            ///////////////////////
+        int  counter = -r+1;
+            for(int i = -r+1;i < r;i++){
+                counter++;
+                s[posY-r][posX-i] = " x ";
+                s[posY+r-2][posX-i] = " x ";
+                s[posY-counter][posX+r-1] = " s ";
+                s[posY-counter][posX-r+1] = " s ";
+                }
+
+        for(int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s.length; j++){
+
+                System.out.print(s[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        String[][] arr = new String[10][10];
+        printArr(arr);
+        printArrRectangle(arr,4,5);
+        printArrRectangles(arr,2,4,6);
+        }
+    }
